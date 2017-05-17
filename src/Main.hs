c5a1f2df588a8e145a3d9a9976fdf97be5008f97
Will Fancher <elvishjerricco@gmail.com>
2017-05-17 18:35:27 -0400

    Added '-Dverbose' to 'mvn', and handled some errors better

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,10 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
 import Control.Applicative
 import Control.Monad
 import Control.Monad.IO.Class
-import Control.Monad.Trans.Maybe
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
 import Data.Char
@@ -15,6 +15,8 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Maybe
 import Data.Semigroup
+import Network.HTTP.Conduit hiding (path)
+import Network.HTTP.Types.Status
 import System.Directory
 import System.Environment
 import System.Exit
@@ -23,19 +25,29 @@ import System.IO
 import System.IO.Temp
 import System.Process
 import Text.XML.Light
+import Control.Monad.Reader
+import Control.Monad.Trans.Maybe
+
+data HavenEnv = HavenEnv
+  { _havenEnv_manager :: Manager
+  , _havenEnv_repos :: Map String String
+  , _havenEnv_m2Local :: FilePath
+  }
 
 -- | Takes multiple maven package descriptions as command line arguments
 -- and finds the dependencies of those maven packages.
 -- Package descriptions should be of the form @groupid:artifactid:version@
 main :: IO ()
 main = do
+  mgr <- newManager tlsManagerSettings
   [pomXml] <- getArgs
   Just repos <- fmap parseRepos . parseXMLDoc <$> BS.readFile pomXml
 
-  withSystemTempFile "pom.xml" $ \tmpFile hTmpFile -> withSystemTempDirectory "m2" $ \m2Repo -> do
+  withSystemTempFile "out.txt" $ \tmpFile hTmpFile -> withSystemTempDirectory "m2" $ \m2Repo -> do
+    let havenEnv = HavenEnv mgr repos m2Repo
     hProc <- runProcess
       "mvn"
-      ["-f", pomXml, "dependency:tree", "-DoutputFile=" <> tmpFile, "-Dmaven.repo.local=" <> m2Repo]
+      ["-f", pomXml, "dependency:tree", "-Dverbose", "-DoutputFile=" <> tmpFile, "-Dmaven.repo.local=" <> m2Repo]
       Nothing
       Nothing
       Nothing
@@ -56,8 +68,12 @@ main = do
             version = takeWhile (/=':') mavenVr -- Version number
             maven = Maven groupId artifactId version
         unless (all isSpace line) $ do
-          Just mvnNix <- fetch m2Repo fileType repos maven -- fileType should be used here to only fetch needed files
-          putStrLn $ toNix mvnNix
+          mMvnNix <- runReaderT (fetch fileType maven) havenEnv
+          case mMvnNix of
+            Just mvnNix -> putStrLn $ toNix mvnNix
+            Nothing -> do
+              hPutStrLn stderr $ "Failed for " <> unlines [show fileType, show maven]
+              exitFailure
           loop
   putStrLn "]"
 
@@ -100,50 +116,69 @@ toNix m =
       , "    aarSha256 = " <> showHash (_mavenNix_aarSha256 m) <> "; }"
       ]
 
+getRepo :: (MonadReader HavenEnv m, MonadPlus m) => String -> m String
+getRepo repoId = do
+  repos <- asks _havenEnv_repos
+  maybe empty pure $ Map.lookup repoId repos
+
 -- | Hash a particular maven package's .pom and .jar files and parse the .pom file as xml
-fetch :: FilePath -> String -> Map String String -> Maven -> IO (Maybe MavenNix)
-fetch m2Repo fileType repos mvn = do
-  let m2Dir = foldl (</>) m2Repo
+fetch
+  :: (MonadIO m, MonadReader HavenEnv m)
+  => String
+  -> Maven
+  -> m (Maybe MavenNix)
+fetch fileType mvn = runMaybeT $ do
+  m2Repo <- asks _havenEnv_m2Local
+  let component = foldl (</>) ""
         [ (\x -> if x == '.' then '/' else x) <$> _maven_groupId mvn
         , _maven_artifactId mvn
         , _maven_version mvn
         ]
-      m2File ext = m2Dir </> _maven_artifactId mvn <> "-" <> _maven_version mvn <> ext
+      m2Dir = m2Repo </> component
+      m2Filename ext = _maven_artifactId mvn <> "-" <> _maven_version mvn <> ext
       findRepoId = takeWhile (/='=') . drop 1 . dropWhile (/='>')
 
-  repoId <- findRepoId <$> readFile (m2Dir </> "_remote.repositories")
+  repoId <- findRepoId <$> liftIO (readFile (m2Dir </> "_remote.repositories"))
+  repo <- getRepo repoId
 
-  let pomFile = m2File ".pom"
-      jarFile = m2File ".jar"
-      aarFile = m2File ".aar"
+  let shaArtifact ext = do
+        mgr <- asks _havenEnv_manager
+        let path = m2Dir </> m2Filename ext
+        m2ArtifactExists <- liftIO $ doesFileExist path
+        contents <- if m2ArtifactExists then liftIO (BL.readFile path) else do
+          let url = repo </> component </> m2Filename ext
+          req <- liftIO $ parseRequest url
+          liftIO $ hPutStrLn stderr $ "Getting URL: " <> url
+          rsp <- liftIO $ httpLbs req mgr
+          when (responseStatus rsp /= status200) $ do
+            liftIO $ hPutStrLn stderr $ "Failed to get URL: " <> url
+            empty
+          return $ responseBody rsp
+        return $ sha256 contents
 
-  pomExists <- doesFileExist pomFile -- Not all repos have poms, though this seems to be exceptionally rare
-  pomSha <- if pomExists then Just . sha256 <$> BL.readFile pomFile else return Nothing
+  pomSha <- runMaybeT $ shaArtifact ".pom"
+  
+  let noArtifacts = MavenNix
+        { _mavenNix_maven = mvn
+        , _mavenNix_repo = repo
+        , _mavenNix_jarSha256 = Nothing
+        , _mavenNix_pomSha256 = pomSha
+        , _mavenNix_aarSha256 = Nothing
+        }
 
-  runMaybeT $ do
-    repo <- maybe empty pure $ Map.lookup repoId repos
-
-    let noArtifacts = MavenNix
-          { _mavenNix_maven = mvn
-          , _mavenNix_repo = repo
-          , _mavenNix_jarSha256 = Nothing
-          , _mavenNix_pomSha256 = pomSha
-          , _mavenNix_aarSha256 = Nothing
-          }
-
-    -- TODO: Match the 'type' to the correct file extension.
-    -- The extension is _usually_ equal to the type, but it's not necessarily.
-    -- See: https://maven.apache.org/pom.html#Dependencies
-    asum
-      [ do
-        guard (fileType == "jar")
-        jarSha <- sha256 <$> liftIO (BL.readFile jarFile)
-        return $ noArtifacts { _mavenNix_jarSha256 = Just jarSha }
-      , do
-        guard (fileType == "aar")
-        aarSha <- sha256 <$> liftIO (BL.readFile aarFile)
-        return $ noArtifacts { _mavenNix_jarSha256 = Just aarSha }
-      ]
+  -- TODO: Match the 'type' to the correct file extension.
+  -- The extension is _usually_ equal to the type, but it's not necessarily.
+  -- See: https://maven.apache.org/pom.html#Dependencies
+  asum
+    [ do
+      guard (fileType == "jar")
+      jarSha <- shaArtifact ".jar"
+      return $ noArtifacts { _mavenNix_jarSha256 = Just jarSha }
+    , do
+      guard (fileType == "aar")
+      aarSha <- shaArtifact ".aar"
+      return $ noArtifacts { _mavenNix_jarSha256 = Just aarSha }
+    ]
 
 -- | Retrieve an XML Element's children by tag name
 findChildrenByTagName :: String -> Element -> [Element]
