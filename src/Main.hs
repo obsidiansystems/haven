28ad8d0231610b3059fd08f4c49a1588216f941a
Will Fancher <elvishjerricco@gmail.com>
2017-05-17 02:18:39 -0400

    Use Maven itself to solve the dependencies.
    This is not only much more accurate, but also easier to implement.

--- src/Main.hs
+++ src/Main.hs
@@ -1,29 +1,73 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import Control.Monad.State
+import Control.Applicative
+import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Maybe
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BL
+import Data.Char
 import Data.Digest.Pure.SHA
+import Data.Foldable
+import Data.Function
+import Data.Map (Map)
+import qualified Data.Map as Map
 import Data.Maybe
 import Data.Semigroup
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Network.HTTP.Conduit hiding (path)
-import Network.HTTP.Types.Status
+import System.Directory
 import System.Environment
+import System.Exit
+import System.FilePath
+import System.IO
+import System.IO.Temp
+import System.Process
 import Text.XML.Light
-
-import Debug.Trace
 
 -- | Takes multiple maven package descriptions as command line arguments
 -- and finds the dependencies of those maven packages.
 -- Package descriptions should be of the form @groupid:artifactid:version@
 main :: IO ()
 main = do
-  mavens <- getArgs
-  mgr <- newManager tlsManagerSettings
-  (_, deps) <- flip runStateT Set.empty $ traverse (recurseDependencies mgr) $ parseMaven <$> mavens
-  putStrLn $ "[" <> (concatMap toNix deps)  <> "]"
+  [pomXml] <- getArgs
+  Just repos <- fmap parseRepos . parseXMLDoc <$> BS.readFile pomXml
 
+  withSystemTempFile "pom.xml" $ \tmpFile hTmpFile -> withSystemTempDirectory "m2" $ \m2Repo -> do
+    hProc <- runProcess
+      "mvn"
+      ["-f", pomXml, "dependency:tree", "-DoutputFile=" <> tmpFile, "-Dmaven.repo.local=" <> m2Repo]
+      Nothing
+      Nothing
+      Nothing
+      (Just stderr)
+      Nothing
+    ExitSuccess <- waitForProcess hProc
+
+    _ <- hGetLine hTmpFile -- skip local package name
+    putStrLn "["
+    fix $ \loop -> do
+      e <- hIsEOF hTmpFile
+      unless e $ do
+        line <- hGetLine hTmpFile
+        let mavenGrArTyVr = dropWhile (not . isAlphaNum) line -- Skip leading symbols; we don't care about parsing this
+            (groupId, ':':mavenArTyVr) = break (==':') mavenGrArTyVr
+            (artifactId, ':':mavenTyVr) = break (==':') mavenArTyVr
+            (fileType, ':':mavenVr) = break (==':') mavenTyVr -- File type that Maven has decided on
+            version = takeWhile (/=':') mavenVr -- Version number
+            maven = Maven groupId artifactId version
+        unless (all isSpace line) $ do
+          Just mvnNix <- fetch m2Repo fileType repos maven -- fileType should be used here to only fetch needed files
+          putStrLn $ toNix mvnNix
+          loop
+  putStrLn "]"
+
+parseRepos :: Element -> Map String String
+parseRepos pom = Map.fromList $ do
+  repoList <- findChildrenByTagName "repositories" pom
+  repo <- findChildrenByTagName "repository" repoList
+  repoId <- findChildrenByTagName "id" repo
+  repoUrl <- findChildrenByTagName "url" repo
+  return (strContent repoId, strContent repoUrl)
 
 data Maven = Maven
   { _maven_groupId :: String
@@ -41,14 +85,6 @@ data MavenNix = MavenNix
   }
   deriving (Show, Eq, Ord)
 
--- | Parses strings of the form @groupid:artifactid:version@,
--- e.g., @com.android.build.tools:gradle:2.3.0@
-parseMaven :: String -> Maven
-parseMaven s =
-  let (groupId, _:rest) = break (==':') s
-      (artifactId, _:version) = break (==':') rest
-  in Maven groupId artifactId version
-
 -- | Create a nix record for a hashed maven package
 toNix :: MavenNix -> String
 toNix m =
@@ -64,148 +100,50 @@ toNix m =
       , "    aarSha256 = " <> showHash (_mavenNix_aarSha256 m) <> "; }"
       ]
 
--- | Maven repositories in which to look for packages
-mirrors :: [String]
-mirrors =
-  [ "https://repo1.maven.org/maven2/"
-  , "https://jcenter.bintray.com/"
-  , "http://central.maven.org/maven2/"
-  , "https://maven.atlassian.com/3rdparty/"
-  ]
-
 -- | Hash a particular maven package's .pom and .jar files and parse the .pom file as xml
-fetch :: [String] -> Manager -> Maven -> IO (Maybe (MavenNix, Element))
-fetch (mirror:fallbacks) mgr mvn = do
-  let version = takeWhile (/= ',') $ dropWhile (\c -> c == '[' || c == '(') $ _maven_version mvn
-      url ext = mconcat
-        [ mirror
-        , (\x -> if x == '.' then '/' else x) <$> _maven_groupId mvn
-        , "/"
+fetch :: FilePath -> String -> Map String String -> Maven -> IO (Maybe MavenNix)
+fetch m2Repo fileType repos mvn = do
+  let m2Dir = foldl (</>) m2Repo
+        [ (\x -> if x == '.' then '/' else x) <$> _maven_groupId mvn
         , _maven_artifactId mvn
-        , "/"
-        , version
-        , "/"
-        , _maven_artifactId mvn
-        , "-"
-        , version
-        , ext
+        , _maven_version mvn
         ]
-      hash rsp = if responseStatus rsp == status200
-                   then Just $ sha256 $ responseBody rsp
-                   else Nothing
-  reqPom <- parseRequest $ trace (show $ url ".pom") $ url ".pom"
-  reqJar <- parseRequest $ url ".jar"
-  reqAar <- parseRequest $ url ".aar"
-  pom <- httpLbs reqPom mgr
-  jar <- httpLbs reqJar mgr
-  aar <- httpLbs reqAar mgr
-  if responseStatus jar /= status200 && responseStatus pom /= status200 && responseStatus aar /= status200
-    then fetch fallbacks mgr mvn
-    else return $
-      let Just e = parseXMLDoc $ responseBody pom
-          mvnNix = MavenNix
-            { _mavenNix_maven = mvn
-            , _mavenNix_repo = mirror
-            , _mavenNix_jarSha256 = hash jar
-            , _mavenNix_pomSha256 = hash pom
-            , _mavenNix_aarSha256 = hash aar
-            }
-      in Just (mvnNix, e)
-fetch [] _ mvn = trace (show $ mconcat
-  [ "Error: Could not find "
-  , _maven_groupId mvn
-  , ":"
-  , _maven_artifactId mvn
-  , ":"
-  , _maven_version mvn
-  , " in any mirror."
-  ]) $ return Nothing
+      m2File ext = m2Dir </> _maven_artifactId mvn <> "-" <> _maven_version mvn <> ext
+      findRepoId = takeWhile (/='=') . drop 1 . dropWhile (/='>')
 
--- | XML pom files may reference values defined elsewhere in the XML
--- using the syntax @${toplevelnode.childnode.grandchildnode}@.
--- They may also refer to values in the @properties@ node
-strContentLookup :: Element -> String -> Maybe String
-strContentLookup e path = case path of
-  ('$':'{':rest) ->
-    let path' = takeWhile (/='}') rest
-        segments = words $ fmap (\c -> if c == '.' then ' ' else c) path'
-        lookupSegment :: Maybe Element -> String -> Maybe Element
-        lookupSegment parent seg = case parent of
-          Just el -> firstChildByTagName seg el
-          Nothing -> Nothing
-        lookupSegments = foldl lookupSegment (Just e)
-    in
-      fmap strContent $ case segments of
-        ("project":segs) -> case lookupSegments segments of
-          Just str -> Just str
-          -- The interpolator seems to fall back to looking things
-          -- up in the @parent@ tag if they are not found as direct
-          -- children of the project tag
-          Nothing -> case lookupSegments $ "parent":segs of
-            Just str -> Just str
-            -- The interpolator also looks things up in the @properties
-            -- tag, but in a slightly different way. The properties tag
-            -- is only one level deep and the keys of its children
-            -- are of the form @x.y.z@
-            Nothing -> do
-              props <- firstChildByTagName "properties" e
-              prop <- firstChildByTagName path' props
-              return prop
-        _ -> lookupSegments segments
-  _ -> Nothing
+  repoId <- findRepoId <$> readFile (m2Dir </> "_remote.repositories")
 
-strContentWithInterpolation :: Element -> Element -> Maybe String
-strContentWithInterpolation pom e = case strContent e of
-  path@('$':_) -> strContentLookup pom path
-  str -> Just str
+  let pomFile = m2File ".pom"
+      jarFile = m2File ".jar"
+      aarFile = m2File ".aar"
 
--- | Extract maven package information from an XML element
--- (usually in a @dependency@ tag)
-mavenFromXml :: Element -> Element -> [Maven]
-mavenFromXml pom dep = catMaybes $ do
-  groupId <- findChildrenByTagName "groupId" dep
-  artifactId <- findChildrenByTagName "artifactId" dep
-  version <- findChildrenByTagName "version" dep
-  let optional = case findChildrenByTagName "optional" dep of
-        [] -> False
-        xs -> any ((=="true") . strContent) xs
-  guard $ not optional
-  return $ do
-    g <- strContentWithInterpolation pom groupId
-    a <- strContentWithInterpolation pom artifactId
-    v <- strContentWithInterpolation pom version
-    return $ Maven g a v
+  pomExists <- doesFileExist pomFile -- Not all repos have poms, though this seems to be exceptionally rare
+  pomSha <- if pomExists then Just . sha256 <$> BL.readFile pomFile else return Nothing
 
--- | Extract the dependencies from a package's pom xml
-getDepsFor :: Element -> [Maven]
-getDepsFor x = do
-  deps <- findChildrenByTagName "dependencies" x
-  dep <- findChildrenByTagName "dependency" deps
-  mavenFromXml x dep
+  runMaybeT $ do
+    repo <- maybe empty pure $ Map.lookup repoId repos
 
--- | Extract parent maven packages from a package's pom xml
-getParentsFor :: Element -> [Maven]
-getParentsFor x = do
-  parents <- findChildrenByTagName "parent" x
-  mavenFromXml x parents
+    let noArtifacts = MavenNix
+          { _mavenNix_maven = mvn
+          , _mavenNix_repo = repo
+          , _mavenNix_jarSha256 = Nothing
+          , _mavenNix_pomSha256 = pomSha
+          , _mavenNix_aarSha256 = Nothing
+          }
 
--- getSubmodulesFor :: Element -> [Maven]
--- getSubmodulesFor x = do
-
--- | Given a starting maven package, retrieve its dependencies recursively
-recurseDependencies :: Manager -> Maven -> StateT (Set MavenNix) IO ()
-recurseDependencies mgr mvn = do
-  s <- get
-  when (not $ any (\mvnNix -> _mavenNix_maven mvnNix == mvn) s) $ do
-    x <- liftIO $ fetch mirrors mgr mvn
-    case x of
-      Nothing -> return ()
-      Just (mvnNix, e) -> do
-        modify $ Set.insert mvnNix
-        -- liftIO $ putStrLn $ "Deps for " <> _maven_artifactId mvn
-        -- liftIO $ print (getDepsFor e <> getParentsFor e)
-        void $ traverse (recurseDependencies mgr) $
-          getDepsFor e <> getParentsFor e
+    -- TODO: Match the 'type' to the correct file extension.
+    -- The extension is _usually_ equal to the type, but it's not necessarily.
+    -- See: https://maven.apache.org/pom.html#Dependencies
+    asum
+      [ do
+        guard (fileType == "jar")
+        jarSha <- sha256 <$> liftIO (BL.readFile jarFile)
+        return $ noArtifacts { _mavenNix_jarSha256 = Just jarSha }
+      , do
+        guard (fileType == "aar")
+        aarSha <- sha256 <$> liftIO (BL.readFile aarFile)
+        return $ noArtifacts { _mavenNix_jarSha256 = Just aarSha }
+      ]
 
 -- | Retrieve an XML Element's children by tag name
 findChildrenByTagName :: String -> Element -> [Element]
